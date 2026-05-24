5fe74d2f901608fc88d1f31ce06fa773758b3cd0
YLiang <yliang01@test.com>
2026-05-25 03:03:38 +0800

    commit

--- start.cmd
+++ start.cmd
@@ -2,6 +2,5 @@
 setlocal
 set "APP_DIR=%~dp0"
 cd /d "%APP_DIR%"
-java -jar "%APP_DIR%hive-oss.jar" ^
-  --spring.config.additional-location="file:%APP_DIR%config/external-secret.yml" ^
+java -jar "%APP_DIR%hive-oss.jar"
 pause
