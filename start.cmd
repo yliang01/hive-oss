b4c020d2c794cef772fff7c93ff59de4a8e8f8ff
YLiang <yliang01@test.com>
2026-05-25 02:47:54 +0800

    commit

--- start.cmd
+++ start.cmd
@@ -4,7 +4,4 @@ set "APP_DIR=%~dp0"
 cd /d "%APP_DIR%"
 java -jar "%APP_DIR%hive-oss.jar" ^
   --spring.config.additional-location="file:%APP_DIR%config/external-secret.yml" ^
-  --hive.imageCacheDir="%APP_DIR%data/image-cache/" ^
-  --hive.imageCacheTtlDays=7 ^
-  --hive.downloadDir="%APP_DIR%data/download/" ^
 pause
